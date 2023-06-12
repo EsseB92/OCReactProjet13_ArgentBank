fb5fe4a886e20e607e04dbad3cb02c3ab71cffd3
Bertrand Sudre <sudrebertrand@gmail.com>
2023-06-12 16:21:47 +0200

    fix: duplication de code

--- front/src/components/Form/RegisterForm.jsx
+++ front/src/components/Form/RegisterForm.jsx
@@ -11,11 +11,43 @@ const initialState = {
     confirmPassword: '',
 };
 
+const inputList = [
+    {
+        id: 'firstName',
+        type: 'text',
+        autoComplete: 'given-name',
+        label: 'First Name',
+    },
+    {
+        id: 'lastName',
+        type: 'text',
+        autoComplete: 'family-name',
+        label: 'Last Name',
+    },
+    {
+        id: 'email',
+        type: 'email',
+        autoComplete: 'email',
+        label: 'Email',
+    },
+    {
+        id: 'password',
+        type: 'password',
+        autoComplete: 'new-password',
+        label: 'Password',
+    },
+    {
+        id: 'confirmPassword',
+        type: 'password',
+        autoComplete: 'new-password',
+        label: 'Confirm Password',
+    },
+];
+
 const RegisterForm = () => {
     document.title = 'Register';
 
     const [formValue, setFormValue] = useState(initialState);
-    const { firstName, lastName, email, password, confirmPassword } = formValue;
 
     const handleChange = (event) => {
         setFormValue({ ...formValue, [event.target.name]: event.target.value });
@@ -29,61 +61,19 @@ const RegisterForm = () => {
         <form onSubmit={handleRegister}>
             <i className={`fa fa-user-circle ${styles.icon}`}></i>
             <h1>Register</h1>
-            <div className={styles.input_wrapper}>
-                <label htmlFor='firstName'>First Name</label>
-                <input
-                    id='firstName'
-                    type='text'
-                    name='firstName'
-                    value={firstName}
-                    onChange={handleChange}
-                    autoComplete='given-name'
-                />
-            </div>
-            <div className={styles.input_wrapper}>
-                <label htmlFor='lastName'>Last Name</label>
-                <input
-                    id='lastName'
-                    type='text'
-                    name='lastName'
-                    value={lastName}
-                    onChange={handleChange}
-                    autoComplete='family-name'
-                />
-            </div>
-            <div className={styles.input_wrapper}>
-                <label htmlFor='email'>Email</label>
-                <input
-                    id='email'
-                    type='email'
-                    name='email'
-                    value={email}
-                    onChange={handleChange}
-                    autoComplete='email'
-                />
-            </div>
-            <div className={styles.input_wrapper}>
-                <label htmlFor='password'>Password</label>
-                <input
-                    id='password'
-                    type='password'
-                    name='password'
-                    value={password}
-                    onChange={handleChange}
-                    autoComplete='new-password'
-                />
-            </div>
-            <div className={styles.input_wrapper}>
-                <label htmlFor='confirmPassword'>Confirm Password</label>
-                <input
-                    id='confirmPassword'
-                    type='password'
-                    name='confirmPassword'
-                    value={confirmPassword}
-                    onChange={handleChange}
-                    autoComplete='new-password'
-                />
-            </div>
+            {inputList.map(({ id, type, autoComplete, label }) => (
+                <div key={id} className={styles.input_wrapper}>
+                    <label htmlFor={id}>{label}</label>
+                    <input
+                        id={id}
+                        type={type}
+                        name={id}
+                        value={formValue[id]}
+                        onChange={handleChange}
+                        autoComplete={autoComplete}
+                    />
+                </div>
+            ))}
             <button type='submit'>Register</button>
         </form>
     );
